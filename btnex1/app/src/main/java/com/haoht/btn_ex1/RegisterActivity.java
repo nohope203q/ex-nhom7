6c575c1cf3df5f31cef15a928cfda6a0da5a1e7f
nohope203q <hq301119@gmail.com>
2025-11-10 22:22:03 +0700

    first commit

---
+++ btnex1/app/src/main/java/com/haoht/btn_ex1/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.haoht.btn_ex1;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_register);
+    }
+
+}
